9110dba0f8fc3f18157e3d8986215008280ad69e
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2021-01-16 09:58:21 +0500

    bug fixed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React, {Component} from 'react';
 import './App.css';
 import Navbar from './navbar/navbar';
 import Footer from './footer/footer';
-
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-
 import Home from './home/home';
 import Fundraising from './fundraising/fundraising';
 import UserProfile from './userProfile/userProfile';
 import Auction from './auction/auction';
 
 class App extends Component {
-
   render() {
 
     return (
